c498bdf678d68e43022bb8b22df97914886b5842
yaroslav.putria <yaroslav.putria@avid.com>
2016-08-29 16:55:25 +0300

    2 travis test

--- test/webdriver-test/test.js
+++ test/webdriver-test/test.js
@@ -3,15 +3,16 @@ global.expect = chai.expect;
 chai.Should();
 describe('Webdriver tests:', function () {
   it('must be 5 files with chosen criteria', function () {
-    browser.url('http://172.22.195.20:8080');
-    browser.waitForExist('#userName');
-    browser.setValue('#userName', 'my8bit');
-    browser.selectByAttribute('#type', 'value', 'text/plain');
-    browser.selectByAttribute('#lang', 'value', 'Markdown');
-    browser.click('#sendRequest');
-    browser.waitForExist('#listOfFiles');
-    var lis = browser.elements('#listOfFiles li');
-    lis.value.length.should.be.equal(5);
+    browser.url('http://172.22.195.20:8080').then(function () {
+        browser.waitForExist('#userName');
+        browser.setValue('#userName', 'my8bit');
+        browser.selectByAttribute('#type', 'value', 'text/plain');
+        browser.selectByAttribute('#lang', 'value', 'Markdown');
+        browser.click('#sendRequest');
+        browser.waitForExist('#listOfFiles');
+        var lis = browser.elements('#listOfFiles li');
+        lis.value.length.should.be.equal(5);
+    });
   });
 });
 
